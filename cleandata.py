698d7b81b7258a62ccc88a5af180d95a9a2c5fa0
Ky Phuong <ttkphuong.9d@gmail.com>
2022-10-10 00:58:51 +0700

    journal

--- cleandata.py
+++ cleandata.py
@@ -3,7 +3,8 @@ import json
 
 # Opening JSON file
 jsonObj = pd.read_json(path_or_buf='C:/Users/ttkph/Documents/cslt/data_NCKH/cs-train.jsonl', lines=True)
-
-print(jsonObj.head)
+journal = jsonObj['journal']
+print(journal.head(1)) #in ra 5 hang dau tien
+#jsonObj.columns  # in ten cac cot
 # # displaying data
 # df.to_csv('C:/Users/ttkph/Desktop/dataclean.csv', index=False, encoding='utf-8')
