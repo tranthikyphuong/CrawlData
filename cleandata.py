96dcd0d698c804778511253b5a2d115320cb0edb
Ky Phuong <ttkphuong.9d@gmail.com>
2022-10-10 11:20:33 +0700

    plot

--- cleandata.py
+++ cleandata.py
@@ -1,10 +1,18 @@
 import pandas as pd
+import numpy as np
 import json
-
-# Opening JSON file
+import matplotlib.pyplot as plt
+from collections import Counter
+    # Opening JSON file
 jsonObj = pd.read_json(path_or_buf='C:/Users/ttkph/Documents/cslt/data_NCKH/cs-train.jsonl', lines=True)
 journal = jsonObj['journal']
-print(journal.head(1)) #in ra 5 hang dau tien
-#jsonObj.columns  # in ten cac cot
-# # displaying data
-# df.to_csv('C:/Users/ttkph/Desktop/dataclean.csv', index=False, encoding='utf-8')
+Count = Counter(journal)
+df = pd.DataFrame({'amount':Count})
+df.plot()
+plt.show()
+    # print(journal.head(1)) #in ra 5 hang dau tien
+    # print(jsonObj.dtypes)
+
+    #jsonObj.columns  # in ten cac cot
+    # # displaying data
+    # jsonObj.to_csv('C:/Users/ttkph/Desktop/dataclean.csv', index=False, encoding='utf-8')
